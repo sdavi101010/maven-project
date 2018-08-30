00879dc12a126eae34591ba49635317a06c6d91c
Scott Davidson <scott@scott-davidson.com>
2018-08-30 14:20:49 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,7 +18,7 @@ public class Greeter {
    *
    * @return the greeting
    */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
