b10916dd40637677c4e30612a1ae180876eb1e27
Scott Davidson <scott@scott-davidson.com>
2018-08-30 14:19:10 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,6 +15,8 @@ public class Greeter {
   /**
    * Provides a greeting.
    * @param someone a descriptor to whom you are greeting.
+   *
+   * @return the greeting
    */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
